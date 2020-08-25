f600cab297b68746d5575ba4ec2786ea2aeeb0fc
David Plain <you@example.com>
2020-08-25 14:34:53 +1000

    randomness added

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,16 @@
-def primary():
-  print("Keep it logically awesome")
+import random
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-  #test
-  #print(quotes)
+def primary():
+  #print("Keep it logically awesome")
+
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  last = 13
+  rnd = random.randint(0, last)
+  
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
